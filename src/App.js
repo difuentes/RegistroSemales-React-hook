edfc7989bdf3c931349c2b82288270b14dec685f
Diego Emilio Fuentes Gomez <fuentesdiego054@gmail.com>
2020-01-08 11:29:51 -0300

    Primer Commit inicio del proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App container ">
+      <header><h1>Gastos Semanales</h1></header>
+
+    </div>
+  );
+}
+
+export default App;
